360bacd562663b9ca1fde0ce155c3d322197da35
Kaywuyep <wuyepkate@gmail.com>
2024-01-15 05:12:38 -0800

    updated 7

--- models/user.py
+++ models/user.py
@@ -4,6 +4,10 @@ from models.base_model import BaseModel, Base
 from sqlalchemy import Column
 from sqlalchemy import String
 from sqlalchemy.orm import relationship
+from os import getenv
+
+
+Storage = getenv("HBNB_TYPE_STORAGE")
 
 
 class User(BaseModel, Base):
@@ -23,8 +27,17 @@ class User(BaseModel, Base):
             with a back reference to "user" and cascade "delete".
     """
     __tablename__ = "users"
-    email = Column(String(128), nullable=False)
-    password = Column(String(128), nullable=False)
-    first_name = Column(String(128))
-    last_name = Column(String(128))
-    places = relationship("Place", backref="user", cascade="delete")
+    if Storage = "db":
+        email = Column(String(128), nullable=False)
+        password = Column(String(128), nullable=False)
+        first_name = Column(String(128), nullable=True)
+        last_name = Column(String(128), nullable=True)
+        places = relationship(
+                "Place",
+                backref="user",
+                cascade="all, delete-orphan")
+    else:
+        email = ""
+        password = ""
+        first_name = ""
+        last_name = ""
