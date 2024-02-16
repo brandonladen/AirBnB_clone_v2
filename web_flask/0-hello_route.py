fbbf678076765261cda36e5e62688df8fc4b2c59
brandonladen <brandonladen486@gmail.com>
2024-02-16 10:41:24 +0300

    added /hbnb route

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -5,13 +5,20 @@ from flask import Flask
 
 app = Flask(__name__)
 @app.route('/', strict_slashes=False)
-
-def hello():
+def hello_hbnb():
     """
     Routing to root, strict_slashes ensure
     the URL works when it ends both with or without the /
     """
     return "Hello HBNB!"
+
+@app.route('/hbnb', strict_slashes=False)
+def hello_bnb():
+    """
+    Routing to root, strict_slashes ensure
+    the URL works when it ends both with or without the /
+    """
+    return "HBNB"
 
 
 if __name__ == "__main__":
