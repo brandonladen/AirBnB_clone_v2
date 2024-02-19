e094be281eb1d91aa450db0e67189b5ce4542737
brandonladen <brandonladen486@gmail.com>
2024-02-19 04:30:05 +0300

    addeda file

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+"""Start a Flask web application"""
+
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+
+app = Flask(__name__)
+
+app.route('/states_list/', strict_slashes=False)
+def states_list():
+    """Displays a HTML page with a list of all states"""
+    states = storage.all(State)
+    return render_template('7-states_list.html', states=states)
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000, debug=True)
