6b59b38cf37431a7137083b9637710f14a820261
brandonladen <brandonladen486@gmail.com>
2024-02-19 04:59:37 +0300

    updates

--- web_flask/10-hbnb_filters.py
+++ web_flask/10-hbnb_filters.py
@@ -4,22 +4,25 @@
 from flask import Flask, render_template
 from models import storage
 from models.state import State
+from models.amenity import Amenity
 
 
 app = Flask(__name__)
-
-
-@app.route('/states_list', strict_slashes=False)
-def states_list():
-    """Displays a HTML page with a list of all states"""
-    states = storage.all(State)
-    return render_template('7-states_list.html', states=states)
 
 
 @app.teardown_appcontext
 def teardown(self):
     """removes the current SQLAlchemy Session"""
     storage.close()
+
+
+@app.route('/hbnb_filters', strict_slashes=False)
+def hbnb_filters():
+    """Displays HBnB filters page"""
+    states = storage.all(State)
+    amenities = storage.all(Amenity)
+    return render_template("10-hbnb_filters.html",
+                           states=states, amenities=amenities)
 
 
 if __name__ == "__main__":
